e93103c63c9ad3395c172b99a238824614dd1e91
NonimUdara <nonimudara123@gmail.com>
2025-08-21 00:34:46 +0530

    Add user details to login response for improved client-side handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,7 +58,14 @@ export function loginUser(req, res) {
 
                     res.json({
                         message: "Login successful",
-                        token: token
+                        token: token,
+                        user: {
+                            email: user.email,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            role: user.role,
+                            isEmailVerified: user.isEmailVerified,
+                        }
                     });
                 } else {
                     res.status(500).json({
