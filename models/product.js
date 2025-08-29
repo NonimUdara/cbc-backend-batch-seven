67361bfa86baa06758560c9e1a4757b73dea9e63
NonimUdara <nonimudara123@gmail.com>
2025-08-29 16:50:12 +0530

    Fix typo in product schema: corrected 'lablledPrice' to 'labledPrice'

--- models/product.js
+++ models/product.js
@@ -29,7 +29,7 @@ const productSchema = new mongoose.Schema(
             type : Number,
             required : true
         },
-        lablledPrice : {
+        labledPrice : {
             type : Number,
             required : true
         },
