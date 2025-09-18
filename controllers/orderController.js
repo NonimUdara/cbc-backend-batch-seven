a46dde5bb24fe2b87455c8cbf844a2507429067c
NonimUdara <nonimudara123@gmail.com>
2025-09-18 20:22:37 +0530

    update order controller with relavent funtionalaties.

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,61 +1,136 @@
 import Order from "../models/order.js";
+import Product from "../models/product.js";
 
 export async function createOrder(req, res) {
+  //CBC0000001
 
-    //CBC0000001
+  // if(req.user == null) {
+  //     res.status(401).json({
+  //         message: "Please login and try again"
+  //     })
+  //     return
+  // }
 
-    // if(req.user == null) {
-    //     res.status(401).json({
-    //         message: "Please login and try again"
-    //     })
-    //     return
-    // }
-
-    try{
-
-        const orderList = await Order.find().sort({date : -1}).limit(1);
-
-        let newOrderID = "CBC0000001";
-        
-        if(orderList.length !== 0) {
-
-            let lastOrderIDInString = orderList[0].oderID; //CBC0000001
-            let lastOrderNumberInString = lastOrderIDInString.replace("CBC", ""); //0000001
-            let lastOrderNumber = parseInt(lastOrderNumberInString); //0000001
-            let newOrderNumber = lastOrderNumber + 1; //0000002
-            //pad start
-            let newOrderNumberInString = newOrderNumber.toString().padStart(7, "0"); //0000002
-            
-            newOrderID = "CBC" + newOrderNumberInString; //CBC0000002
-        }
-
-        const newOrder = new Order({
-            oderID : newOrderID,
-            items : [],
-            customerName : req.body.customerName,
-            email : req.body.email,
-            address : req.body.address,
-            phone : req.body.phone,
-            total : req.body.total,
-            status : "Pending",
-            date: new Date()
-        });
-        
-        const savedOrder = await newOrder.save();
-
-        res.status(201).json(
-            {
-            message: "Order created successfully",
-            order: savedOrder
-        }
-    )
-
-    } catch(err) {
-
-        console.error("Error creating order:", err);
-        res.status(500).json({
-            error: "Failed to create order"
-        });
+  try {
+    const user = req.user;
+    if (user == null) {
+      res.status(401).json({
+        message: "unauthorized user",
+      });
+      return;
     }
 
+    const orderList = await Order.find().sort({ date: -1 }).limit(1);
+
+    let newOrderID = "CBC0000001";
+
+    if (orderList.length !== 0) {
+      let lastOrderIDInString = orderList[0].oderID; //CBC0000001
+      let lastOrderNumberInString = lastOrderIDInString.replace("CBC", ""); //0000001
+      let lastOrderNumber = parseInt(lastOrderNumberInString); //0000001
+      let newOrderNumber = lastOrderNumber + 1; //0000002
+      //pad start
+      let newOrderNumberInString = newOrderNumber.toString().padStart(7, "0"); //0000002
+
+      newOrderID = "CBC" + newOrderNumberInString; //CBC0000002
+    }
+
+    let customerName = req.body.customerName;
+    if (customerName == null) {
+      customerName = user.firstName + " " + user.lastName;
+    }
+
+    let phone = req.body.phone;
+    if (phone == null) {
+      phone = "Not provided";
+    }
+
+    const itemsInRequest = req.body.items;
+
+    if (itemsInRequest.length == null) {
+      res.status(400).json({
+        message: "Items are required to place an order",
+      });
+      return;
+    }
+
+    if (!Array.isArray(itemsInRequest)) {
+      res.status(400).json({
+        message: "Items must be an array",
+      });
+      return;
+    }
+
+    const itemsToBeAdded = [];
+    let total = 0;
+
+    for (let i = 0; i < itemsInRequest.length; i++) {
+      const item = itemsInRequest[i];
+
+      const product = await Product.findOne({ productID: item.productID });
+
+      if (product == null) {
+        res.status(400).json({
+          code : "not-found",
+          message: `Product not found with ID ${item.productID}`,
+          productID: item.productID,
+        });
+        return;
+      }
+
+      if (product.stock < item.quantity) {
+        res.status(400).json(
+          {
+            code : "stock",
+            message: `Product with ID ${item.productID} is out of stock`,
+            productID: item.productID,
+            availableStock: product.stock
+          }
+        );
+        return;
+      }
+
+      itemsToBeAdded.push({
+        productID: product.productID,
+        quantity: item.quantity,
+        name: product.name,
+        price: product.price,
+        image: product.images[0]
+      });
+
+      total += product.price * item.quantity;
+
+    }
+
+    const newOrder = new Order({
+      oderID: newOrderID,
+      items: itemsToBeAdded,
+      customerName: customerName,
+      email: user.email,
+      address: req.body.address,
+      phone: phone,
+      total: total,
+    });
+
+    const savedOrder = await newOrder.save();
+
+    // update stock
+    // for (let i = 0; i < itemsToBeAdded.length; i++) {
+    //   const item = itemsToBeAdded[i];
+    //   await Product.updateOne(
+    //     { productID: item.productID },
+    //     { $inc: { stock: -item.quantity } }
+    //   )
+    // }
+
+    res.status(201).json({
+      message: "Order created successfully",
+      order: savedOrder,
+    });
+  } catch (err) {
+    console.error("Error creating order:", err);
+    res.status(500).json({
+      error: "Failed to create order",
+    });
+  }
 }
